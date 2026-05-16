0d5e75400715732bd07177deb82c3b4a092fd251
Knight Witch <amanda.m.ivans@gmail.com>
2026-05-15 21:13:43 -0700

    Add builder config scaffold

---
+++ fourthwall/builder/kwb-builder-config.js
@@ -0,0 +1,61 @@
+window.KWB_CONFIG={
+  version:"Dev_v0.1.0",
+  storefrontToken:null,
+  shopDomain:null,
+  currency:"USD",
+  assets:{
+    agencyFontWoff2:"https://knightwitch.nyc3.cdn.digitaloceanspaces.com/FONTS/agencyfb-webfont.woff2",
+    agencyFontWoff:"https://knightwitch.nyc3.cdn.digitaloceanspaces.com/FONTS/agencyfb-webfont.woff",
+    tradeGothicFontWoff2:null,
+    tradeGothicFontWoff:null,
+    coverVideo:null,
+    outroVideo:null,
+    backgroundImage:null
+  },
+  coreCollections:{
+    basscraft:"basscraft-core",
+    blackmass:"blackmass-core",
+    edgerunners:"edgerunners-core",
+    wickedhearts:"wickedhearts-core",
+    astralplane:"astralplane-core",
+    starchild:"starchild-core"
+  },
+  jacketCollections:{
+    men:{
+      jackets:null,
+      coats:null,
+      vests:null
+    },
+    women:{
+      jackets:null,
+      coats:null,
+      vests:null
+    },
+    neutral:{
+      jackets:null,
+      coats:null,
+      vests:null
+    }
+  },
+  accent:{
+    enabled:false,
+    manifestUrl:"./data/accent-eligibility.json"
+  },
+  afterpay:{
+    enabled:true,
+    installments:4,
+    label:"afterpay"
+  },
+  copy:{
+    coverTitle:"Design Your Jacket",
+    coverLines:["Your Vision","Your Style","Begins Here"],
+    coverCta:"Start My Design",
+    coreTitle:"Choose Your Design",
+    jacketTitle:"Select Your Jacket Style",
+    accentTitle:"Pick Your Accent Colour",
+    reviewTitle:"Confirm Your Spellweave",
+    confirmationTitle:"Added To Cart!",
+    confirmationBody:"Finish your checkout process & our witches will start your Spellweave!",
+    viewCart:"View Cart"
+  }
+};
