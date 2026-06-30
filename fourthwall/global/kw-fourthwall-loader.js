31a02608065694efdd766bad4e5efc35c097e25a
Knight Witch <amanda.m.ivans@gmail.com>
2026-06-29 21:54:20 -0700

    Bump carousel controller version

--- fourthwall/global/kw-fourthwall-loader.js
+++ fourthwall/global/kw-fourthwall-loader.js
@@ -7,7 +7,7 @@
   const repo = m ? m[1] : "Knight-Witch/kw-site-widgets";
   const selfRef = m ? m[2] : "main";
   const base = `https://cdn.jsdelivr.net/gh/${repo}`;
-  const version = s?.dataset.version || "20260629-carousel-scroll-3";
+  const version = s?.dataset.version || "20260629-carousel-scroll-4";
 
   const manifest =
     s?.dataset.productMediaManifest ||
