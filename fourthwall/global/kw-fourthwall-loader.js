0de2b178480bf6f1128ad50b9c0068e9cda50da7
Knight Witch <amanda.m.ivans@gmail.com>
2026-07-17 20:30:19 -0700

    Bump ladies size guide cache version

--- fourthwall/global/kw-fourthwall-loader.js
+++ fourthwall/global/kw-fourthwall-loader.js
@@ -7,7 +7,7 @@
   const repo = m ? m[1] : "Knight-Witch/kw-site-widgets";
   const selfRef = m ? m[2] : "main";
   const base = `https://cdn.jsdelivr.net/gh/${repo}`;
-  const version = s?.dataset.version || "20260717-size-guide-placement-2";
+  const version = s?.dataset.version || "20260717-ladies-size-guides-1";
 
   const manifest =
     s?.dataset.productMediaManifest ||
